6a4724f00fb509508a1803fe6fd64e9d76c5732e
Peter Steinberg <bklynpeter@gmail.com>
2022-06-08 12:32:42 -0400

    get running again with Marshall

--- backend_tests/App.test.js
+++ backend_tests/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "React";
-import App from "../client/App.jsx";
+import App from "../client/App";
 
 /**
  * @jest-environment jsdom
